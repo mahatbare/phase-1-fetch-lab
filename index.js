d86350aef916ec51b4e1f4075d32fcbf9dbe7ed5
mahatbare <mahatbare1240@gmail.com>
2024-04-18 14:16:35 +0300

    index.html

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+ 
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ return fetch("https://anapioficeandfire.com/api/books")
+.then((resp) => resp.json())
+.then((json) => renderBooks(json));
+ 
+ }
   
-}
-
+ 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -10,8 +14,8 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+ 
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
